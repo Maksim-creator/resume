eb31887feeedd10e0e00052edb6768c82e413143
Maksim <maksimka.bocharov11@gmail.com>
2021-02-22 17:51:32 +0200

    html commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,10 @@
-import accordeon from "./modules/accordeon";
-import modal from './modules/modal';
+// import accordeon from "./modules/accordeon";
+// import modal from './modules/modal';
 
 window.addEventListener('DOMContentLoaded', () => {
-    accordeon('.accordion-heading');
-    modal('.nav_btn', '.overlay');
+    // accordeon('.accordion-heading');
+    // modal('.nav_btn', '.overlay');
+    window.addEventListener('click', e => {
+        console.log(e.target);
+    })
 })
